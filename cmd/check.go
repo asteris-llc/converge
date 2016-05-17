ea9a7895972a4a3510601fbd744d990e569225b6
Brian Hicks <brian@brianthicks.com>
2016-05-17 10:08:42 -0500

    module: add

---
+++ cmd/check.go
@@ -0,0 +1,57 @@
+// Copyright © 2016 Asteris, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"errors"
+	"io/ioutil"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/asteris-llc/converge/module"
+	"github.com/spf13/cobra"
+)
+
+// checkCmd represents the check command
+var checkCmd = &cobra.Command{
+	Use:   "check",
+	Short: "check that the syntax of a module file is valid",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return errors.New("Need at least one module filename as argument, got 0")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		for _, fname := range args {
+			logger := logrus.WithField("filename", fname)
+
+			content, err := ioutil.ReadFile(fname)
+			if err != nil {
+				logger.WithError(err).Fatal("could not read file")
+			}
+
+			_, err = module.New(content)
+			if err != nil {
+				logger.WithError(err).Fatal("could not parse file")
+			}
+
+			logger.Info("module valid")
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(checkCmd)
+}
