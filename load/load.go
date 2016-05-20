40fd32865c81a77290cf0d7287ce98d934e47e0b
Brian Hicks <brian@brianthicks.com>
2016-05-20 11:32:51 -0500

    load: load module references recursively

--- load/load.go
+++ load/load.go
@@ -26,6 +26,41 @@ import (
 // Load a module from a resource. This uses the protocol in the path (or file://
 // if not present) to determine from where the module should be loaded.
 func Load(source string) (*resource.Module, error) {
+	initial, err := loadAny(source)
+	if err != nil {
+		return initial, err
+	}
+
+	modules := []*resource.Module{initial}
+	for len(modules) > 0 {
+		// bookkeeping to avoid recursive calls. Using `range` here would copy and
+		// not process any new items.
+		var module *resource.Module
+		module, modules = modules[0], modules[1:]
+
+		// actual work - loading modules recursively and replacing their ModuleTasks
+		// with Modules
+		for i, res := range module.Resources {
+			if mt, ok := res.(*resource.ModuleTask); ok {
+				newModule, err := loadAny(mt.Source)
+				if err != nil {
+					return initial, err
+				}
+
+				newModule.Args = mt.Args
+				newModule.Source = mt.Source
+				newModule.ModuleName = mt.ModuleName
+
+				module.Resources[i] = newModule
+				modules = append(modules, newModule)
+			}
+		}
+	}
+
+	return initial, err
+}
+
+func loadAny(source string) (*resource.Module, error) {
 	var (
 		protocol string
 		path     string
