575aefca66b946135768386d8bdbdc56a0fa3959
Brian Hicks <brian@brianthicks.com>
2016-05-24 10:29:17 -0500

    exec: change Graph to AcyclicGraph

---
+++ exec/exec_test.go
@@ -0,0 +1,36 @@
+// Copyright © 2016 Asteris, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package exec_test
+
+import (
+	"testing"
+
+	"github.com/asteris-llc/converge/exec"
+	"github.com/asteris-llc/converge/resource"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	mod := &resource.Module{
+		Resources: []resource.Resource{
+			new(resource.ShellTask),
+		},
+	}
+
+	_, err := exec.New(mod)
+	assert.NoError(t, err)
+}
