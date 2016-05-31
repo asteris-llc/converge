d28579375d27be452975a2ce57468a3d04a5a6fb
Brian Hicks <brian@brianthicks.com>
2016-05-31 17:13:20 -0500

    exec: add planning

---
+++ exec/check.go
@@ -0,0 +1,69 @@
+// Copyright © 2016 Asteris, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package exec
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/asteris-llc/converge/load"
+	"github.com/asteris-llc/converge/resource"
+)
+
+// PlanResult contains the result of a resource check
+type PlanResult struct {
+	Path          string
+	CurrentStatus string
+	WillChange    bool
+}
+
+func (p *PlanResult) String() string {
+	return fmt.Sprintf(
+		"%s:\n\tCurrently: %q\n\tWill Change: %t",
+		p.Path,
+		p.CurrentStatus,
+		p.WillChange,
+	)
+}
+
+// Plan the operations to be performed
+func Plan(graph *load.Graph) (results []*PlanResult, err error) {
+	lock := new(sync.Mutex)
+
+	err = graph.Walk(func(path string, res resource.Resource) error {
+		monitor, ok := res.(resource.Monitor)
+		if !ok {
+			return nil
+		}
+
+		var (
+			err    error
+			result = &PlanResult{Path: path}
+		)
+
+		result.CurrentStatus, result.WillChange, err = monitor.Check()
+		if err != nil {
+			return err
+		}
+
+		lock.Lock()
+		defer lock.Unlock()
+		results = append(results, result)
+
+		return nil
+	})
+
+	return results, err
+}
