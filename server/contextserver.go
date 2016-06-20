6679ccb5d30345376e613c62942ebb60349a4543
Brian Hicks <brian@brianthicks.com>
2016-06-20 15:12:10 -0500

    server: rename to contextserver

--- server/contextserver.go
+++ server/contextserver.go
@@ -23,21 +23,21 @@ import (
 	"golang.org/x/net/context"
 )
 
-// Server whose runtime is controlled by a context for stopping gracefully
-type Server struct {
+// ContextServer is a Server controlled by a context for stopping gracefully
+type ContextServer struct {
 	ctx context.Context
 }
 
-// New constructs and returns a server. Mux has the same meaning as in the
-// standard HTTP package (that is, if nil it will use the globally registered
-// handlers)
-func New(ctx context.Context) *Server {
-	return &Server{ctx}
+// NewContextServer constructs and returns a server. Mux has the same meaning as
+// in the standard HTTP package (that is, if nil it will use the globally
+// registered handlers)
+func NewContextServer(ctx context.Context) *ContextServer {
+	return &ContextServer{ctx}
 }
 
 // ListenAndServe does the same thing as the net/http equivalent, except
 // using the context.
-func (s *Server) ListenAndServe(addr string, handler http.Handler) error {
+func (s *ContextServer) ListenAndServe(addr string, handler http.Handler) error {
 	server := manners.NewWithServer(&http.Server{Addr: addr, Handler: handler})
 	go s.close(server)
 
@@ -46,14 +46,14 @@ func (s *Server) ListenAndServe(addr string, handler http.Handler) error {
 
 // ListenAndServeTLS does the same thing as the net/http equivalent, except
 // using the context.
-func (s *Server) ListenAndServeTLS(addr, certFile, keyFile string, handler http.Handler) error {
+func (s *ContextServer) ListenAndServeTLS(addr, certFile, keyFile string, handler http.Handler) error {
 	server := manners.NewWithServer(&http.Server{Addr: addr, Handler: handler})
 	go s.close(server)
 
 	return server.ListenAndServeTLS(certFile, keyFile)
 }
 
-func (s *Server) close(server *manners.GracefulServer) {
+func (s *ContextServer) close(server *manners.GracefulServer) {
 	<-s.ctx.Done()
 	log.Println("[INFO] gracefully stopping server")
 	server.BlockingClose()
