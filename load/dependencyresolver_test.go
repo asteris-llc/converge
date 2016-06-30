87d8ce716ca233b5ecef66ce81117b17ac637a16
Brian Hicks <brian@brianthicks.com>
2016-06-30 11:32:47 -0500

    load(dependencyresolver): add

---
+++ load/dependencyresolver_test.go
@@ -0,0 +1,47 @@
+// Copyright © 2016 Asteris, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package load_test
+
+import (
+	"testing"
+
+	"github.com/asteris-llc/converge/helpers"
+	"github.com/asteris-llc/converge/load"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDependencyResolverResolvesDependencies(t *testing.T) {
+	defer helpers.HideLogs(t)()
+
+	nodes, err := load.Nodes("../samples/basicDependencies.hcl")
+	require.NoError(t, err)
+
+	resolved, err := load.ResolveDependencies(nodes)
+	assert.NoError(t, err)
+	assert.Contains(t, resolved.DownEdges("root/task.render"), "root/task.directory")
+}
+
+func TestDependencyResolverBadDependency(t *testing.T) {
+	defer helpers.HideLogs(t)()
+
+	nodes, err := load.Nodes("../samples/errors/bad_requirement.hcl")
+	require.NoError(t, err)
+
+	_, err = load.ResolveDependencies(nodes)
+	if assert.Error(t, err) {
+		assert.EqualError(t, err, "nonexistent vertices in edges: root/task.nonexistent")
+	}
+}
