bc594bf22ea24bcbf8d1765be4d969221786eca8
Rebecca Skinner <rebecca@aster.is>
2016-07-20 10:54:54 -0500

    initial prettyprintter passing test

---
+++ prettyprinters/graphviz/graphviz_test.go
@@ -0,0 +1,34 @@
+// Copyright © 2016 Asteris, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package graphviz_test
+
+import (
+	"testing"
+
+	"github.com/asteris-llc/converge/prettyprinters/graphviz"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NewGraphvizPrinter_UsesDefaultOptionsWhenMissing(t *testing.T) {
+	emptyOptsMap := map[string]string{}
+	printer, _ := graphviz.NewPrinter(emptyOptsMap, stubPrinter)
+	actual := printer.Options()
+	expected := graphviz.DefaultOptions()
+	assert.Equal(t, actual, expected)
+}
+
+func stubPrinter(_ interface{}) (string, error) {
+	return "", nil
+}
