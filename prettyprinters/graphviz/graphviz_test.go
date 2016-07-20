9fe8f0c078b904a550dc9b8edf89ebacc40131ab
Rebecca Skinner <rebecca@aster.is>
2016-07-20 12:48:52 -0500

    drawnode checks for marked subgraphs

--- prettyprinters/graphviz/graphviz_test.go
+++ prettyprinters/graphviz/graphviz_test.go
@@ -17,17 +17,19 @@ package graphviz_test
 import (
 	"testing"
 
+	"github.com/asteris-llc/converge/graph"
 	"github.com/asteris-llc/converge/prettyprinters/graphviz"
 	"github.com/stretchr/testify/assert"
 )
 
 var (
 	emptyOptsMap = map[string]string{}
+	emptyGraph   = graph.New()
 )
 
 func Test_NewGraphvizPrinter_WhenMissingOptions_UsesDefaultOptions(t *testing.T) {
 
-	printer, _ := graphviz.NewPrinter(emptyOptsMap, stubPrinter)
+	printer := graphviz.NewPrinter(emptyOptsMap, stubPrinter, stubMarker)
 	actual := printer.Options()
 	expected := graphviz.DefaultOptions()
 	assert.Equal(t, actual, expected)
@@ -35,12 +37,76 @@ func Test_NewGraphvizPrinter_WhenMissingOptions_UsesDefaultOptions(t *testing.T)
 
 func Test_NewGraphvizPrinter_WhenProvidedOptions_UsesProvidedOptions(t *testing.T) {
 	opts := map[string]string{"rankdir": "TB"} // not the default
-	printer, _ := graphviz.NewPrinter(opts, stubPrinter)
+	printer := graphviz.NewPrinter(opts, stubPrinter, stubMarker)
 	setOpts := printer.Options()
 	rankdir := setOpts["rankdir"]
 	assert.Equal(t, "TB", rankdir)
 }
 
+func Test_DrawNode_WhenRenderFunction_CallsRenderFunction(t *testing.T) {
+	calledFlag := false
+	renderF := func(_ interface{}) (string, error) {
+		calledFlag = true
+		return "", nil
+	}
+	printer := graphviz.NewPrinter(emptyOptsMap, renderF, stubMarker)
+	printer.DrawNode(emptyGraph, 1, stubFlagFunc)
+	assert.True(t, calledFlag)
+}
+
+func Test_DrawNode_CallsShouldMark(t *testing.T) {
+	calledFlag := false
+	shouldMark := func(_ interface{}) bool {
+		calledFlag = true
+		return false
+	}
+	g := testGraph()
+	printer := graphviz.NewPrinter(emptyOptsMap, nil, shouldMark)
+	printer.DrawNode(g, 1, stubFlagFunc)
+	assert.True(t, calledFlag)
+}
+
+func Test_DrawNode_WhenMarkerReturnsTrue_CallsSubgraphMark(t *testing.T) {
+	calledFlag := false
+	subgraphMarker := func(_ *graph.Graph) {
+		calledFlag = true
+	}
+	shouldMark := func(_ interface{}) bool { return true }
+	g := testGraph()
+	printer := graphviz.NewPrinter(emptyOptsMap, nil, shouldMark)
+	printer.DrawNode(g, 1, subgraphMarker)
+	assert.True(t, calledFlag)
+}
+
+func Test_DrawNode_WhenMarkerReturnsFalse_NotCallSubgraphMark(t *testing.T) {
+	calledFlag := false
+	subgraphMarker := func(_ *graph.Graph) {
+		calledFlag = true
+	}
+	shouldMark := func(_ interface{}) bool { return false }
+	g := testGraph()
+	printer := graphviz.NewPrinter(emptyOptsMap, nil, shouldMark)
+	printer.DrawNode(g, 1, subgraphMarker)
+	assert.False(t, calledFlag)
+}
+
+func stubFlagFunc(_ *graph.Graph) {
+}
+
 func stubPrinter(_ interface{}) (string, error) {
 	return "", nil
+}
+
+func stubMarker(_ interface{}) bool {
+	return false
+}
+
+func testGraph() *graph.Graph {
+	g := graph.New()
+	g.Add("root", nil)
+	g.Add("child1", nil)
+	g.Add("child2", nil)
+	g.Connect("root", "child1")
+	g.Connect("root", "child2")
+	return g
 }
