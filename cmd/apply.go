0972780ce576e8cdacb5a20e3c7314872ab9337b
Brian Hicks <brian@brianthicks.com>
2016-07-22 14:34:03 -0500

    render: use context

--- cmd/apply.go
+++ cmd/apply.go
@@ -59,7 +59,7 @@ real happens.`,
 				log.Fatalf("[FATAL] %s: could not parse file: %s\n", fname, err)
 			}
 
-			rendered, err := render.Render(graph, params)
+			rendered, err := render.Render(ctx, graph, params)
 			if err != nil {
 				log.Fatalf("[FATAL] %s: could not render: %s\n", fname, err)
 			}
