8f81bcd354b6eb44fa1b147400cc60ff5c69b264
Brian Hicks <brian@brianthicks.com>
2016-07-29 09:52:26 -0500

    blackbox: test that required param fails

---
+++ blackbox/test_required_param.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+ROOT=$(pwd)
+
+TMP=$(mktemp -d -t converge.apply.XXXXXXXXXX)
+function finish {
+    rm -rf $TMP
+}
+trap finish EXIT
+
+pushd $TMP
+
+echo 'param "test" {}' > required_param.hcl
+
+if $ROOT/converge apply required_param.hcl; then
+    echo "failed: apply without required param succeeded"
+    exit 1
+else
+    echo "success: apply without required param failed"
+fi
+
+popd
