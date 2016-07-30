ae13e2bff61df43a2eec4488a37811ebf72e0ae6
Brian Hicks <brian@brianthicks.com>
2016-07-30 15:16:31 -0500

    vendor: add concurrent-map

---
+++ vendor/github.com/streamrail/concurrent-map/concurrent_map_test.go
@@ -0,0 +1,318 @@
+package cmap
+
+import (
+	"encoding/json"
+	"sort"
+	"strconv"
+	"testing"
+)
+
+type Animal struct {
+	name string
+}
+
+func TestMapCreation(t *testing.T) {
+	m := New()
+	if m == nil {
+		t.Error("map is null.")
+	}
+
+	if m.Count() != 0 {
+		t.Error("new map should be empty.")
+	}
+}
+
+func TestInsert(t *testing.T) {
+	m := New()
+	elephant := Animal{"elephant"}
+	monkey := Animal{"monkey"}
+
+	m.Set("elephant", elephant)
+	m.Set("monkey", monkey)
+
+	if m.Count() != 2 {
+		t.Error("map should contain exactly two elements.")
+	}
+}
+
+func TestInsertAbsent(t *testing.T) {
+	m := New()
+	elephant := Animal{"elephant"}
+	monkey := Animal{"monkey"}
+
+	m.SetIfAbsent("elephant", elephant)
+	if ok := m.SetIfAbsent("elephant", monkey); ok {
+		t.Error("map set a new value even the entry is already present")
+	}
+}
+
+func TestGet(t *testing.T) {
+	m := New()
+
+	// Get a missing element.
+	val, ok := m.Get("Money")
+
+	if ok == true {
+		t.Error("ok should be false when item is missing from map.")
+	}
+
+	if val != nil {
+		t.Error("Missing values should return as null.")
+	}
+
+	elephant := Animal{"elephant"}
+	m.Set("elephant", elephant)
+
+	// Retrieve inserted element.
+
+	tmp, ok := m.Get("elephant")
+	elephant = tmp.(Animal) // Type assertion.
+
+	if ok == false {
+		t.Error("ok should be true for item stored within the map.")
+	}
+
+	if &elephant == nil {
+		t.Error("expecting an element, not null.")
+	}
+
+	if elephant.name != "elephant" {
+		t.Error("item was modified.")
+	}
+}
+
+func TestHas(t *testing.T) {
+	m := New()
+
+	// Get a missing element.
+	if m.Has("Money") == true {
+		t.Error("element shouldn't exists")
+	}
+
+	elephant := Animal{"elephant"}
+	m.Set("elephant", elephant)
+
+	if m.Has("elephant") == false {
+		t.Error("element exists, expecting Has to return True.")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	m := New()
+
+	monkey := Animal{"monkey"}
+	m.Set("monkey", monkey)
+
+	m.Remove("monkey")
+
+	if m.Count() != 0 {
+		t.Error("Expecting count to be zero once item was removed.")
+	}
+
+	temp, ok := m.Get("monkey")
+
+	if ok != false {
+		t.Error("Expecting ok to be false for missing items.")
+	}
+
+	if temp != nil {
+		t.Error("Expecting item to be nil after its removal.")
+	}
+
+	// Remove a none existing element.
+	m.Remove("noone")
+}
+
+func TestCount(t *testing.T) {
+	m := New()
+	for i := 0; i < 100; i++ {
+		m.Set(strconv.Itoa(i), Animal{strconv.Itoa(i)})
+	}
+
+	if m.Count() != 100 {
+		t.Error("Expecting 100 element within map.")
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	m := New()
+
+	if m.IsEmpty() == false {
+		t.Error("new map should be empty")
+	}
+
+	m.Set("elephant", Animal{"elephant"})
+
+	if m.IsEmpty() != false {
+		t.Error("map shouldn't be empty.")
+	}
+}
+
+func TestIterator(t *testing.T) {
+	m := New()
+
+	// Insert 100 elements.
+	for i := 0; i < 100; i++ {
+		m.Set(strconv.Itoa(i), Animal{strconv.Itoa(i)})
+	}
+
+	counter := 0
+	// Iterate over elements.
+	for item := range m.Iter() {
+		val := item.Val
+
+		if val == nil {
+			t.Error("Expecting an object.")
+		}
+		counter++
+	}
+
+	if counter != 100 {
+		t.Error("We should have counted 100 elements.")
+	}
+}
+
+func TestBufferedIterator(t *testing.T) {
+	m := New()
+
+	// Insert 100 elements.
+	for i := 0; i < 100; i++ {
+		m.Set(strconv.Itoa(i), Animal{strconv.Itoa(i)})
+	}
+
+	counter := 0
+	// Iterate over elements.
+	for item := range m.IterBuffered() {
+		val := item.Val
+
+		if val == nil {
+			t.Error("Expecting an object.")
+		}
+		counter++
+	}
+
+	if counter != 100 {
+		t.Error("We should have counted 100 elements.")
+	}
+}
+
+func TestItems(t *testing.T) {
+	m := New()
+
+	// Insert 100 elements.
+	for i := 0; i < 100; i++ {
+		m.Set(strconv.Itoa(i), Animal{strconv.Itoa(i)})
+	}
+
+	items := m.Items()
+
+	if len(items) != 100 {
+		t.Error("We should have counted 100 elements.")
+	}
+}
+
+func TestConcurrent(t *testing.T) {
+	m := New()
+	ch := make(chan int)
+	const iterations = 1000
+	var a [iterations]int
+
+	// Using go routines insert 1000 ints into our map.
+	go func() {
+		for i := 0; i < iterations/2; i++ {
+			// Add item to map.
+			m.Set(strconv.Itoa(i), i)
+
+			// Retrieve item from map.
+			val, _ := m.Get(strconv.Itoa(i))
+
+			// Write to channel inserted value.
+			ch <- val.(int)
+		} // Call go routine with current index.
+	}()
+
+	go func() {
+		for i := iterations / 2; i < iterations; i++ {
+			// Add item to map.
+			m.Set(strconv.Itoa(i), i)
+
+			// Retrieve item from map.
+			val, _ := m.Get(strconv.Itoa(i))
+
+			// Write to channel inserted value.
+			ch <- val.(int)
+		} // Call go routine with current index.
+	}()
+
+	// Wait for all go routines to finish.
+	counter := 0
+	for elem := range ch {
+		a[counter] = elem
+		counter++
+		if counter == iterations {
+			break
+		}
+	}
+
+	// Sorts array, will make is simpler to verify all inserted values we're returned.
+	sort.Ints(a[0:iterations])
+
+	// Make sure map contains 1000 elements.
+	if m.Count() != iterations {
+		t.Error("Expecting 1000 elements.")
+	}
+
+	// Make sure all inserted values we're fetched from map.
+	for i := 0; i < iterations; i++ {
+		if i != a[i] {
+			t.Error("missing value", i)
+		}
+	}
+}
+
+func TestJsonMarshal(t *testing.T) {
+	SHARD_COUNT = 2
+	defer func() {
+		SHARD_COUNT = 32
+	}()
+	expected := "{\"a\":1,\"b\":2}"
+	m := New()
+	m.Set("a", 1)
+	m.Set("b", 2)
+	j, err := json.Marshal(m)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(j) != expected {
+		t.Error("json", string(j), "differ from expected", expected)
+		return
+	}
+}
+
+func TestKeys(t *testing.T) {
+	m := New()
+
+	// Insert 100 elements.
+	for i := 0; i < 100; i++ {
+		m.Set(strconv.Itoa(i), Animal{strconv.Itoa(i)})
+	}
+
+	keys := m.Keys()
+	if len(keys) != 100 {
+		t.Error("We should have counted 100 elements.")
+	}
+}
+
+func TestMInsert(t *testing.T) {
+	animals := map[string]interface{}{
+		"elephant": Animal{"elephant"},
+		"monkey":   Animal{"monkey"},
+	}
+	m := New()
+	m.MSet(animals)
+
+	if m.Count() != 2 {
+		t.Error("map should contain exactly two elements.")
+	}
+}
