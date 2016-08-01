0ad59030a64753bbf1011bc81e8375518df8c1fb
Rebecca Skinner <rebecca@aster.is>
2016-08-01 08:56:05 -0500

    Update doc string

--- prettyprinters/graphviz/doc.go
+++ prettyprinters/graphviz/doc.go
@@ -1,4 +1,4 @@
-// package graphviz provides a concrete prettyprinters.DigraphPrettyPrinter
+// Package graphviz provides a concrete prettyprinters.DigraphPrettyPrinter
 // implementation for rendering directed graphs as Graphviz-compatible dot
 // source files.  It exports an interface, graphviz.PrintProvider, that allows
 // users to provide general methods for rendering graphed data types into
