7240498bb2cbcf27a980697b9116719881329be2
Brian Hicks <brian@brianthicks.com>
2016-08-09 17:04:06 -0500

    prettyprinters(human): add basic

---
+++ prettyprinters/human/human_test.go
@@ -0,0 +1,133 @@
+// Copyright © 2016 Asteris, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package human_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/asteris-llc/converge/graph"
+	pp "github.com/asteris-llc/converge/prettyprinters"
+	"github.com/asteris-llc/converge/prettyprinters/human"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSatisfiesInterface(t *testing.T) {
+	t.Parallel()
+
+	assert.Implements(t, (*pp.BasePrinter)(nil), new(human.Printer))
+	assert.Implements(t, (*pp.NodePrinter)(nil), new(human.Printer))
+	assert.Implements(t, (*pp.GraphPrinter)(nil), new(human.Printer))
+}
+
+func testFinishPP(t *testing.T, in Printable, out string) {
+	g := graph.New()
+	g.Add("root", in)
+
+	printer := new(human.Printer)
+	str, err := printer.FinishPP(g)
+
+	require.Nil(t, err)
+	assert.Equal(t, out, str.String())
+}
+
+func TestFinishPPSuccess(t *testing.T) {
+	t.Parallel()
+
+	testFinishPP(
+		t,
+		Printable{"a": "b"},
+		"Summary: 0 errors, 1 changes",
+	)
+}
+
+func TestFinishPPError(t *testing.T) {
+	t.Parallel()
+
+	testFinishPP(
+		t,
+		Printable{"error": "test"},
+		"Summary: 1 errors, 1 changes\n\n * root: test\n",
+	)
+}
+
+func testDrawNodes(t *testing.T, in Printable, out string) {
+	g := graph.New()
+	g.Add("root", in)
+
+	printer := new(human.Printer)
+	str, err := printer.DrawNode(g, "root")
+
+	require.Nil(t, err)
+	assert.Equal(t, out, str.String())
+}
+
+func TestDrawNodeNoChanges(t *testing.T) {
+	t.Parallel()
+
+	testDrawNodes(
+		t,
+		Printable{},
+		"root:\n  Changes:\n    No changes\n\n",
+	)
+}
+
+func TestDrawNodeChanges(t *testing.T) {
+	t.Parallel()
+
+	testDrawNodes(
+		t,
+		Printable{"a": "b"},
+		"root:\n  Changes:\n    a: \"\" => \"b\"\n\n",
+	)
+}
+
+func TestDrawNodeError(t *testing.T) {
+	t.Parallel()
+
+	testDrawNodes(
+		t,
+		Printable{"error": "x"},
+		"root:\n  Error: x\n  Changes:\n    error: \"\" => \"x\"\n\n",
+	)
+}
+
+// printable stub
+
+type Printable map[string]string
+
+func (p Printable) Fields() map[string][2]string {
+	out := map[string][2]string{}
+
+	for key, value := range p {
+		out[key] = [2]string{"", value}
+	}
+
+	return out
+}
+
+func (p Printable) HasChanges() bool {
+	return len(p) > 0
+}
+
+func (p Printable) Error() error {
+	err, ok := p["error"]
+	if !ok {
+		return nil
+	}
+
+	return errors.New(err)
+}
