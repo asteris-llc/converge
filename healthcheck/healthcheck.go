8809fd4ad92f1220dccc339f6353b3c7b54f545c
Rebecca Skinner <rebecca@aster.is>
2016-08-23 10:26:44 -0500

    refactor resource/health.go into healthcheck package

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -1,16 +1,17 @@
-package resource
+package healthcheck
 
 import (
 	"context"
 	"errors"
 
 	"github.com/asteris-llc/converge/graph"
+	"github.com/asteris-llc/converge/resource"
 )
 
 // Check defines the interface for a health check
 type Check interface {
-	FailingDep(string, TaskStatus)
-	HealthCheck() (*HealthStatus, error)
+	FailingDep(string, resource.TaskStatus)
+	HealthCheck() (*resource.HealthStatus, error)
 }
 
 // CheckGraph walks a graph and runs health checks on each health-checkable node
@@ -25,7 +26,7 @@ func CheckGraph(ctx context.Context, in *graph.Graph) (*graph.Graph, error) {
 			return nil
 		}
 		for _, dep := range out.Dependencies(id) {
-			depStatus, ok := out.Get(dep).(TaskStatus)
+			depStatus, ok := out.Get(dep).(resource.TaskStatus)
 			if !ok {
 				continue
 			}
@@ -42,20 +43,21 @@ func CheckGraph(ctx context.Context, in *graph.Graph) (*graph.Graph, error) {
 	})
 }
 
-func unboxNode(i interface{}) (TaskStatus, error) {
+// unboxNode will remove a resource.TaskStatus from a plan.Result or apply.Result
+func unboxNode(i interface{}) (resource.TaskStatus, error) {
 	type statusWrapper interface {
-		GetStatus() TaskStatus
+		GetStatus() resource.TaskStatus
 	}
 
 	if wrapper, ok := i.(statusWrapper); ok {
 		return wrapper.GetStatus(), nil
-	} else if taskStatus, ok := i.(TaskStatus); ok {
+	} else if taskStatus, ok := i.(resource.TaskStatus); ok {
 		return taskStatus, nil
 	}
 	return nil, errors.New("[FATAL] cannot get task status from node")
 }
 
-func isFailingStatus(stat TaskStatus) bool {
+func isFailingStatus(stat resource.TaskStatus) bool {
 	if check, ok := stat.(Check); ok {
 		return healthCheckOK(check)
 	}
