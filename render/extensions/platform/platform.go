b64c3cb4c4812d3c45ec1ec8d2dac8fafb824c6b
Steven Borrelli <steve@aster.is>
2016-08-24 10:43:22 -0500

    Add platform.go to extensions

---
+++ render/extensions/platform/platform.go
@@ -0,0 +1,43 @@
+// Copyright © 2016 Asteris, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//Package platform queries the underlying operating system
+
+package platform
+
+import (
+	"runtime"
+)
+
+type Platform struct {
+	OS                string
+	LinuxDistribution string
+	Name              string
+	PrettyName        string
+	Version           string
+	Like              []string
+	Build             string
+}
+
+func NewPlatform() Platform {
+	var platform Platform
+	platform.OS = runtime.GOOS
+	switch platform.OS {
+	case "darwin":
+		platform.OSXVers()
+	case "linux":
+		platform.LinuxLSB()
+	}
+	return platform
+}
