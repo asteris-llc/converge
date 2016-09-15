b9bcbe911f58b28b223da9f4da381bf62a61cb7e
Steven Borrelli <steve@aster.is>
2016-09-15 15:21:38 -0500

    fix conflict

--- load/resource.go
+++ load/resource.go
@@ -32,6 +32,7 @@ import (
 	_ "github.com/asteris-llc/converge/resource/file/directory"
 	_ "github.com/asteris-llc/converge/resource/file/mode"
 	_ "github.com/asteris-llc/converge/resource/group"
+	_ "github.com/asteris-llc/converge/resource/file"
 	_ "github.com/asteris-llc/converge/resource/module"
 	_ "github.com/asteris-llc/converge/resource/param"
 	_ "github.com/asteris-llc/converge/resource/shell"
