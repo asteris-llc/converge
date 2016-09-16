835dbc66ef520bc006f498786227410d8ce54259
Brian Hicks <brian@brianthicks.com>
2016-09-16 17:01:35 -0500

    cmd(version): bump version number

--- cmd/version.go
+++ cmd/version.go
@@ -21,7 +21,7 @@ import (
 )
 
 // Version describes the version for packaging
-const Version = "0.1.1"
+const Version = "0.2.0-beta1"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
