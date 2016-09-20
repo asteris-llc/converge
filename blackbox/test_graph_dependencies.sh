8cf50ccfde5a3a772ebc6edbe7029d803d171037
Brian Hicks <brian@brianthicks.com>
2016-09-20 10:30:33 -0500

    blackbox: more quoting than that, even

--- blackbox/test_graph_dependencies.sh
+++ blackbox/test_graph_dependencies.sh
@@ -5,17 +5,17 @@ ROOT=$(pwd)
 
 TMP=$(mktemp -d -t converge.graph_dependencies.XXXXXXXXXX)
 function finish {
-    rm -rf ${TMP}
+    rm -rf "$TMP"
 }
 trap finish EXIT
 
-pushd ${TMP}
+pushd "$TMP"
 
-for src in ${ROOT}/samples/testdata/*.hcl; do
-  b=$(basename ${src})
+for src in "$ROOT"/samples/testdata/*.hcl; do
+  b=$(basename "$src")
 	# since panic doesn't happen 100% of the time, run test a few times
   for _ in $(seq 1 5); do
-    ${ROOT}/converge graph --local -l "WARN" ${src} >/dev/null
+    "$ROOT"/converge graph --local -l "WARN" "$src" >/dev/null
 	done
 	echo "success: no errors generating dependencies for ${b}"
 done
