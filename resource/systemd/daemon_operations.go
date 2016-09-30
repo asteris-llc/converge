cc74617396c39344725d3b2c255a34e95a685ce7
David Ayeke <ayekedavid@yahoo.com>
2016-09-30 15:55:51 -0500

    Modified License header for daemon_operations.go
    Added a newline between the end of the header and the package declaration
    for code quality purposes

--- resource/systemd/daemon_operations.go
+++ resource/systemd/daemon_operations.go
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.package systemd
+
 package systemd
 
 // CheckDaemonReload checks whether the systemd daemon needs to be reloaded.
