24d4d0aacd09fcb68bedb4b2c9513adc7f4ede86
Rebecca Skinner <rebecca@aster.is>
2016-10-05 15:20:18 -0500

    add missing license headers

--- render/preprocessor/control/resources.go
+++ render/preprocessor/control/resources.go
@@ -1,3 +1,17 @@
+// Copyright © 2016 Asteris, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package control
 
 import (
@@ -51,6 +65,7 @@ func (s *SwitchTask) Apply() (resource.TaskStatus, error) {
 	return &resource.Status{}, nil
 }
 
+// CasePreparer contains generated case macro information
 type CasePreparer struct {
 	Predicate string `hcl:"predicate"`
 	Name      string `hcl:"name"`
@@ -70,6 +85,8 @@ func (c *CasePreparer) Prepare(r resource.Renderer) (resource.Task, error) {
 	}, nil
 }
 
+// CaseTask represents a task and is used to determine whether a conditional
+// task should evaluate or not
 type CaseTask struct {
 	Predicate string
 	Name      string
