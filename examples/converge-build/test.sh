8dd611b269b08a16d6abd0c4fe3a58c14eb66ca3
Steven Borrelli <steve@aster.is>
2016-10-14 13:08:24 -0500

    remove redhat and update docker invocation

--- examples/converge-build/test.sh
+++ examples/converge-build/test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+#tests setting up a converge build environment on different linux distributions
+
 distro=$1
 
 case "$distro" in 
-"centos" | "redhat" | "fedora")
-    docker run --rm  -v $PWD:/converge ${distro}:latest /converge/converge apply --local /converge/converge-centos.hcl
+"centos" | "fedora")
+    docker run --rm  -v $(pwd):/converge ${distro}:latest /converge/converge apply --local /converge/converge-centos.hcl
     ;;
 "debian" | "ubuntu")
-    docker run --rm  -v $PWD:/converge ${distro}:latest  /converge/converge apply --local /converge/converge-deb-ubuntu.hcl
+    docker run --rm  -v $(pwd):/converge ${distro}:latest  /converge/converge apply --local /converge/converge-deb-ubuntu.hcl
     ;;
 *)
     echo "unsupported distribution: ${distro}"
