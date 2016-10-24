eafd054d80a6d6d1a71cd37af5872ab33bc2b66e
Alexander V. Nikolaev <avn@avnik.info>
2016-10-24 22:29:37 +0000

    lvm: re-format more tests, using t.Run()

--- resource/lvm/lowlevel/query_test.go
+++ resource/lvm/lowlevel/query_test.go
@@ -8,35 +8,39 @@ import (
 	"testing"
 )
 
-func TestBlkid(t *testing.T) {
-	lvm, e := testhelpers.MakeLvmWithMockExec()
-	expected := []string{"-c", "/dev/null", "-o", "value", "-s", "TYPE", "/dev/sda1"}
-	e.On("ReadWithExitCode", "blkid", expected).Return("xfs", 0, nil)
-	fs, err := lvm.Blkid("/dev/sda1")
-	assert.Equal(t, "xfs", fs)
-	assert.NoError(t, err)
-	e.AssertCalled(t, "ReadWithExitCode", "blkid", expected)
+// TestLVMBlkid tests LVM.Blkid()
+func TestLVMBlkid(t *testing.T) {
+	t.Run("normal", func(t *testing.T) {
+		lvm, e := testhelpers.MakeLvmWithMockExec()
+		expected := []string{"-c", "/dev/null", "-o", "value", "-s", "TYPE", "/dev/sda1"}
+		e.On("ReadWithExitCode", "blkid", expected).Return("xfs", 0, nil)
+		fs, err := lvm.Blkid("/dev/sda1")
+		assert.Equal(t, "xfs", fs)
+		assert.NoError(t, err)
+		e.AssertCalled(t, "ReadWithExitCode", "blkid", expected)
+	})
+
+	t.Run("error during blkid call", func(t *testing.T) {
+		lvm, e := testhelpers.MakeLvmWithMockExec()
+		e.On("ReadWithExitCode", "blkid", mock.Anything).Return("", 0, fmt.Errorf("failed"))
+		_, err := lvm.Blkid("/dev/sda1")
+		assert.Error(t, err)
+	})
+
+	t.Run("blkid exit with nonzero (missing device)", func(t *testing.T) {
+		lvm, e := testhelpers.MakeLvmWithMockExec()
+		e.On("ReadWithExitCode", "blkid", mock.Anything).Return("", 2, nil)
+		fs, err := lvm.Blkid("/dev/sda1")
+		assert.NoError(t, err)
+		assert.Equal(t, "", fs)
+	})
 }
 
-func TestBlkidError(t *testing.T) {
-	lvm, e := testhelpers.MakeLvmWithMockExec()
-	e.On("ReadWithExitCode", "blkid", mock.Anything).Return("", 0, fmt.Errorf("failed"))
-	_, err := lvm.Blkid("/dev/sda1")
-	assert.Error(t, err)
-}
-
-func TestBlkidNonzero(t *testing.T) {
-	lvm, e := testhelpers.MakeLvmWithMockExec()
-	e.On("ReadWithExitCode", "blkid", mock.Anything).Return("", 2, nil)
-	fs, err := lvm.Blkid("/dev/sda1")
-	assert.NoError(t, err)
-	assert.Equal(t, "", fs)
-}
-
+// TestQueryParseEmptyString test for LVM.Query{Physical,Logical}Volumes and .VolumeGroups() with empty command output
+// .query() is not exported in interface, so use QueryPhysicalVolumes() which call it under the hood.
 func TestQueryParseEmptyString(t *testing.T) {
 	lvm, e := testhelpers.MakeLvmWithMockExec()
 	e.On("Read", "pvs", mock.Anything).Return("", nil)
-	// FIXME: .Query() is not exported in interface, so use QueryPhysicalVolumes() which go
 	pvs, err := lvm.QueryPhysicalVolumes()
 	assert.NoError(t, err)
 	assert.Empty(t, pvs)
