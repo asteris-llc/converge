f94298da3565dea49af6e8a4971d4ce632d33c4b
Alexander V. Nikolaev <avn@avnik.info>
2016-10-31 14:42:43 +0000

    lvm/vg: Add more tests for VG

---
+++ resource/lvm/testhelpers/lvm.go
@@ -0,0 +1,118 @@
+package testhelpers
+
+import (
+	"github.com/asteris-llc/converge/resource/lvm/lowlevel"
+	"github.com/stretchr/testify/mock"
+)
+
+type FakeLVM struct {
+	mock.Mock
+}
+
+// MakeFakeLvm create fake LVM for test injections
+func MakeFakeLvm() (lowlevel.LVM, *FakeLVM) {
+	lvm := &FakeLVM{}
+	return lvm, lvm
+}
+
+// Check is mock for LVM.Check()
+func (f *FakeLVM) Check() error {
+	return f.Called().Error(0)
+}
+
+// CheckFilesystemTools is mock for LVM.CheckFilesystemTools()
+func (f *FakeLVM) CheckFilesystemTools(fstype string) error {
+	return f.Called(fstype).Error(0)
+}
+
+// QueryLogicalVolumes is mock for LVM.QueryLogicalVolumes
+func (f *FakeLVM) QueryLogicalVolumes(vg string) (map[string]*lowlevel.LogicalVolume, error) {
+	c := f.Called(vg)
+	return c.Get(0).(map[string]*lowlevel.LogicalVolume), c.Error(1)
+}
+
+//  QueryPhysicalVolumes is mock for LVM.QueryPhysicalVolumes()
+func (f *FakeLVM) QueryPhysicalVolumes() (map[string]*lowlevel.PhysicalVolume, error) {
+	c := f.Called()
+	return c.Get(0).(map[string]*lowlevel.PhysicalVolume), c.Error(1)
+}
+
+//  QueryVolumeGroups is mock for LVM.QueryVolumeGroups()
+func (f *FakeLVM) QueryVolumeGroups() (map[string]*lowlevel.VolumeGroup, error) {
+	c := f.Called()
+	return c.Get(0).(map[string]*lowlevel.VolumeGroup), c.Error(1)
+}
+
+//  CreateVolumeGroup is mock for LVM.CreateVolumeGroup()
+func (f *FakeLVM) CreateVolumeGroup(vg string, devs []string) error {
+	return f.Called(vg, devs).Error(0)
+}
+
+//  ExtendVolumeGroup is mock for LVM.ExtendVolumeGroup()
+func (f *FakeLVM) ExtendVolumeGroup(vg string, dev string) error {
+	return f.Called(vg, dev).Error(0)
+}
+
+//  ReduceVolumeGroup is mock for LVM.ReduceVolumeGroup()
+func (f *FakeLVM) ReduceVolumeGroup(vg string, dev string) error {
+	return f.Called(vg, dev).Error(0)
+}
+
+//  CreatePhysicalVolume is mock for LVM.CreatePhysicalVolume()
+func (f *FakeLVM) CreatePhysicalVolume(dev string) error {
+	return f.Called(dev).Error(0)
+}
+
+//  CreateLogicalVolume is mock for LVM.CreateLogicalVolume()
+func (f *FakeLVM) CreateLogicalVolume(group string, volume string, size *lowlevel.LvmSize) error {
+	return f.Called(group, volume, size).Error(0)
+}
+
+//  Mkfs is mock for LVM.Mkfs()
+func (f *FakeLVM) Mkfs(dev string, fstype string) error {
+	return f.Called(dev, fstype).Error(0)
+}
+
+//  Mountpoint is mock for LVM.Mountpoint()
+func (f *FakeLVM) Mountpoint(path string) (bool, error) {
+	c := f.Called(path)
+	return c.Bool(0), c.Error(1)
+}
+
+//  Blkid is mock for LVM.Blkid()
+func (f *FakeLVM) Blkid(dev string) (string, error) {
+	c := f.Called(dev)
+	return c.String(0), c.Error(1)
+}
+
+//  WaitForDevice is mock for LVM.WaitForDevice()
+func (f *FakeLVM) WaitForDevice(path string) error {
+	return f.Called(path).Error(0)
+}
+
+// systemd units
+
+//  CheckUnit is mock for LVM.CheckUnit()
+func (f *FakeLVM) CheckUnit(filename string, content string) (bool, error) {
+	c := f.Called(filename, content)
+	return c.Bool(0), c.Error(1)
+}
+
+//  UpdateUnit is mock for LVM.UpdateUnit()
+func (f *FakeLVM) UpdateUnit(filename string, content string) error {
+	c := f.Called(filename, content)
+	return c.Error(0)
+}
+
+//  StartUnit is mock for LVM.StartUnit()
+func (f *FakeLVM) StartUnit(unitname string) error {
+	return f.Called(unitname).Error(0)
+}
+
+// FIXME: possible unneeded
+
+//  QueryDeviceMapperName is mock for LVM.QueryDeviceMapperName()
+func (f *FakeLVM) QueryDeviceMapperName(dmName string) (string, error) {
+	c := f.Called(dmName)
+	return c.String(0), c.Error(1)
+}
--- resource/lvm/vg/vg_test.go
+++ resource/lvm/vg/vg_test.go
@@ -3,14 +3,115 @@ package vg_test
 import (
 	"github.com/asteris-llc/converge/helpers/comparison"
 	"github.com/asteris-llc/converge/helpers/fakerenderer"
+	"github.com/asteris-llc/converge/resource"
+	"github.com/asteris-llc/converge/resource/lvm/lowlevel"
 	"github.com/asteris-llc/converge/resource/lvm/testdata"
 	"github.com/asteris-llc/converge/resource/lvm/testhelpers"
 	"github.com/asteris-llc/converge/resource/lvm/vg"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 
+	"fmt"
 	"testing"
 )
+
+func TestVGCheck(t *testing.T) {
+	t.Run("check prerequisites failure", func(t *testing.T) {
+		lvm, m := testhelpers.MakeFakeLvm()
+		m.On("Check").Return(fmt.Errorf("failed"))
+		_ = simpleCheckFailure(t, lvm, []string{"/dev/sda1"})
+	})
+
+	t.Run("QueryVolumeGroups failure", func(t *testing.T) {
+		lvm, m := testhelpers.MakeFakeLvm()
+		m.On("Check").Return(nil)
+		m.On("QueryVolumeGroups").Return(make(map[string]*lowlevel.VolumeGroup), nil)
+		m.On("QueryPhysicalVolumes").Return(make(map[string]*lowlevel.PhysicalVolume), fmt.Errorf("failed"))
+		_ = simpleCheckFailure(t, lvm, []string{"/dev/sda1"})
+	})
+
+	t.Run("QueryPhysicalVolumes failure", func(t *testing.T) {
+		lvm, m := testhelpers.MakeFakeLvm()
+		m.On("Check").Return(nil)
+		m.On("QueryVolumeGroups").Return(make(map[string]*lowlevel.VolumeGroup), fmt.Errorf("failed"))
+		m.On("QueryPhysicalVolumes").Return(make(map[string]*lowlevel.PhysicalVolume), nil)
+		_ = simpleCheckFailure(t, lvm, []string{"/dev/sda1"})
+	})
+
+	t.Run("single device", func(t *testing.T) {
+		lvm, m := testhelpers.MakeFakeLvm()
+		m.On("Check").Return(nil)
+		m.On("QueryVolumeGroups").Return(make(map[string]*lowlevel.VolumeGroup), nil)
+		m.On("QueryPhysicalVolumes").Return(make(map[string]*lowlevel.PhysicalVolume), nil)
+
+		status, _ := simpleCheckSuccess(t, lvm, []string{"/dev/sda1"})
+		assert.True(t, status.HasChanges())
+		comparison.AssertDiff(t, status.Diffs(), "vg0", "<not exists>", "/dev/sda1")
+	})
+
+	t.Run("multiple devices", func(t *testing.T) {
+		lvm, m := testhelpers.MakeFakeLvm()
+		m.On("Check").Return(nil)
+		m.On("QueryVolumeGroups").Return(make(map[string]*lowlevel.VolumeGroup), nil)
+		m.On("QueryPhysicalVolumes").Return(make(map[string]*lowlevel.PhysicalVolume), nil)
+
+		status, _ := simpleCheckSuccess(t, lvm, []string{"/dev/sda1", "/dev/sdb1", "/dev/sdc1"})
+		assert.True(t, status.HasChanges())
+		comparison.AssertDiff(t, status.Diffs(), "vg0", "<not exists>", "/dev/sda1, /dev/sdb1, /dev/sdc1")
+	})
+
+	t.Run("device from another VG", func(t *testing.T) {
+		lvm, m := testhelpers.MakeFakeLvm()
+		m.On("Check").Return(nil)
+		m.On("QueryVolumeGroups").Return(map[string]*lowlevel.VolumeGroup{
+			"vg1": &lowlevel.VolumeGroup{Name: "vg1"},
+		}, nil)
+		m.On("QueryPhysicalVolumes").Return(map[string]*lowlevel.PhysicalVolume{
+			"/dev/sda1": &lowlevel.PhysicalVolume{
+				Name:  "/dev/sdb1",
+				Group: "vg1",
+			},
+		}, nil)
+		_ = simpleCheckFailure(t, lvm, []string{"/dev/sda1", "/dev/sdb1", "/dev/sdc1"})
+	})
+}
+
+func TestVGApply(t *testing.T) {
+	t.Run("single device", func(t *testing.T) {
+		lvm, m := testhelpers.MakeFakeLvm()
+		m.On("Check").Return(nil)
+		m.On("QueryVolumeGroups").Return(make(map[string]*lowlevel.VolumeGroup), nil)
+		m.On("QueryPhysicalVolumes").Return(make(map[string]*lowlevel.PhysicalVolume), nil)
+		m.On("CreateVolumeGroup", mock.Anything, mock.Anything).Return(nil)
+
+		_ = simpleApplySuccess(t, lvm, []string{"/dev/sda1"})
+		m.AssertCalled(t, "CreateVolumeGroup", "vg0", []string{"/dev/sda1"})
+	})
+
+	t.Run("multiple devices", func(t *testing.T) {
+		t.Skip() // BUG BUG BUG!!! IDK why, but only 1st device passed to CreateVolumeGroup()
+		lvm, m := testhelpers.MakeFakeLvm()
+		m.On("Check").Return(nil)
+		m.On("QueryVolumeGroups").Return(make(map[string]*lowlevel.VolumeGroup), nil)
+		m.On("QueryPhysicalVolumes").Return(make(map[string]*lowlevel.PhysicalVolume), nil)
+		m.On("CreateVolumeGroup", "vg0", []string{"/dev/sda1", "/dev/sdb1", "/dev/sdc1"}).Return(nil)
+
+		_ = simpleApplySuccess(t, lvm, []string{"/dev/sda1", "/dev/sdb1", "/dev/sdc1"})
+		m.AssertCalled(t, "CreateVolumeGroup", "vg0", []string{"/dev/sda1", "/dev/sdb1", "/dev/sdc1"})
+	})
+
+	t.Run("CreatePhysicalVolume  failure", func(t *testing.T) {
+		lvm, m := testhelpers.MakeFakeLvm()
+		m.On("Check").Return(nil)
+		m.On("QueryVolumeGroups").Return(make(map[string]*lowlevel.VolumeGroup), nil)
+		m.On("QueryPhysicalVolumes").Return(make(map[string]*lowlevel.PhysicalVolume), nil)
+		m.On("CreateVolumeGroup", mock.Anything, mock.Anything).Return(fmt.Errorf("failed"))
+
+		_ = simpleApplyFailure(t, lvm, []string{"/dev/sda1"})
+		m.AssertCalled(t, "CreateVolumeGroup", "vg0", []string{"/dev/sda1"})
+	})
+}
 
 // TestCreateVolume is a full-blown test, using fake engine to trace from high-level
 // graph node vg.resourceVG, to commands passed to LVM tools. It cover only straighforward
@@ -71,4 +172,37 @@ func TestCreateVolume(t *testing.T) {
 		assert.NoError(t, err)
 		assert.False(t, status.HasChanges())
 	})
+}
+
+func simpleCheckFailure(t *testing.T, lvm lowlevel.LVM, devs []string) resource.TaskStatus {
+	r := vg.NewResourceVG(lvm, "vg0", devs)
+	status, err := r.Check(fakerenderer.New())
+	assert.Error(t, err)
+	return status
+}
+
+func simpleCheckSuccess(t *testing.T, lvm lowlevel.LVM, devs []string) (resource.TaskStatus, resource.Task) {
+	r := vg.NewResourceVG(lvm, "vg0", devs)
+	status, err := r.Check(fakerenderer.New())
+	require.NoError(t, err)
+	require.NotNil(t, status)
+	return status, r
+}
+
+func simpleApplySuccess(t *testing.T, lvm lowlevel.LVM, devs []string) resource.TaskStatus {
+	checkStatus, vg := simpleCheckSuccess(t, lvm, []string{"/dev/sda1"})
+	require.True(t, checkStatus.HasChanges())
+
+	status, err := vg.Apply()
+	require.NoError(t, err)
+	return status
+}
+
+func simpleApplyFailure(t *testing.T, lvm lowlevel.LVM, devs []string) resource.TaskStatus {
+	checkStatus, vg := simpleCheckSuccess(t, lvm, []string{"/dev/sda1"})
+	require.True(t, checkStatus.HasChanges())
+
+	status, err := vg.Apply()
+	require.Error(t, err)
+	return status
 }
