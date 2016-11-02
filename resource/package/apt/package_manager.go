6ac318a6551e7ded77d40d79434a400e6c03db6e
Steven Borrelli <steve@aster.is>
2016-11-02 17:08:46 -0500

    add logic for dpkg install checks

--- resource/package/apt/package_manager.go
+++ resource/package/apt/package_manager.go
@@ -16,8 +16,24 @@ package apt
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/asteris-llc/converge/resource/package"
+)
+
+// Outputs from dpkg-query
+const (
+	// PkgHold means package is installed and held to a version
+	PkgHold = "hold ok installed"
+
+	// PkgInstalled means package is installed normally
+	PkgInstalled = "install ok installed"
+
+	// PkgRemoved means package has been removed, but not purged (config files still present)
+	PkgRemoved = "deinstall ok config-files"
+
+	// PkgUninstalled means package has been uninstalled
+	PkgUninstalled = "unknown ok not-installed"
 )
 
 // Manager provides a concrete implementation of PackageManager for debian
@@ -28,12 +44,29 @@ type Manager struct {
 
 // InstalledVersion gets the installed version of package, if available
 func (y *Manager) InstalledVersion(p string) (pkg.PackageVersion, bool) {
-	result, err := y.Sys.Run(fmt.Sprintf("dpkg -s %s", p))
+	var version string
+	var installed bool
+
+	result, err := y.Sys.Run(fmt.Sprintf("dpkg-query -W -f'${Package},${Status},${Version}\n' %s", p))
 	exitCode, _ := pkg.GetExitCode(err)
 	if exitCode != 0 {
 		return "", false
 	}
-	return (pkg.PackageVersion)(result), true
+	for _, line := range strings.Split(strings.TrimSpace(string(result)), "\n") {
+		l := strings.Split(line, ",")
+		status, ver := l[1], l[2]
+
+		// if any package is not installed, return immediately
+		if strings.HasPrefix(status, PkgRemoved) || strings.HasPrefix(status, PkgUninstalled) {
+			return "", false
+		}
+
+		if strings.HasPrefix(status, PkgInstalled) || strings.HasPrefix(status, PkgHold) {
+			version = ver
+			installed = true
+		}
+	}
+	return (pkg.PackageVersion)(version), installed
 }
 
 // InstallPackage installs a package, returning an error if something went wrong
