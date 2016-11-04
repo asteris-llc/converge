04e9ca8150bf041ba8201a20f72d3e6b7747ae7e
ryane <ryanesc@gmail.com>
2016-11-04 08:11:13 -0400

    docker.network: ipam config support

--- resource/docker/network/network.go
+++ resource/docker/network/network.go
@@ -37,6 +37,9 @@ const (
 
 	// DefaultDriver is the default network driver
 	DefaultDriver = "bridge"
+
+	// DefaultIPAMDriver is the default IPAM driver
+	DefaultIPAMDriver = "default"
 )
 
 // Network is responsible for managing docker volumes
@@ -48,6 +51,7 @@ type Network struct {
 	Driver  string
 	Labels  map[string]string
 	Options map[string]interface{}
+	IPAM    dc.IPAMOptions
 	State   State
 	Force   bool
 }
@@ -65,9 +69,7 @@ func (n *Network) Check(resource.Renderer) (resource.TaskStatus, error) {
 	n.AddDifference(n.Name, string(networkState(nw)), string(n.State), "")
 
 	if n.State == StatePresent && nw != nil && n.Force {
-		n.AddDifference("labels", mapCompareStr(nw.Labels), mapCompareStr(n.Labels), "")
-		n.AddDifference("driver", nw.Driver, n.Driver, DefaultDriver)
-		n.AddDifference("options", mapCompareStr(nw.Options), mapCompareStr(toStrMap(n.Options)), "")
+		n.diffNetwork(nw)
 	}
 
 	if resource.AnyChanges(n.Differences) {
@@ -111,7 +113,9 @@ func (n *Network) Apply() (resource.TaskStatus, error) {
 			Driver:  n.Driver,
 			Labels:  n.Labels,
 			Options: n.Options,
+			IPAM:    n.IPAM,
 		}
+
 		nw, err = n.client.CreateNetwork(opts)
 		if err != nil {
 			n.RaiseLevel(resource.StatusFatal)
@@ -140,6 +144,24 @@ func (n *Network) SetClient(client docker.NetworkClient) {
 	n.client = client
 }
 
+func (n *Network) diffNetwork(nw *dc.Network) {
+	n.AddDifference("labels", mapCompareStr(nw.Labels), mapCompareStr(n.Labels), "")
+	n.AddDifference("driver", nw.Driver, n.Driver, DefaultDriver)
+	n.AddDifference("options", mapCompareStr(nw.Options), mapCompareStr(toStrMap(n.Options)), "")
+	n.AddDifference("ipam_driver", nw.IPAM.Driver, n.IPAM.Driver, DefaultIPAMDriver)
+
+	// we cannot reliably detect a diff of the ipam config if the desired ipam
+	// config is the default ([]) but the actual ipam config has a single
+	// customized entry
+	if len(n.IPAM.Config) > 0 || len(nw.IPAM.Config) > 1 {
+		actualIPAMConfigs := IPAMConfigs(nw.IPAM.Config)
+		sort.Sort(actualIPAMConfigs)
+		expectedIPAMConfigs := IPAMConfigs(n.IPAM.Config)
+		sort.Sort(expectedIPAMConfigs)
+		n.AddDifference("ipam_config", actualIPAMConfigs.String(), expectedIPAMConfigs.String(), "")
+	}
+}
+
 func networkState(nw *dc.Network) State {
 	if nw != nil {
 		return StatePresent
@@ -164,4 +186,47 @@ func mapCompareStr(m map[string]string) string {
 	)
 	sort.Strings(pairs)
 	return strings.Join(pairs, ", ")
+}
+
+// IPAMConfigs is a slice of dc.IPAMConfig
+type IPAMConfigs []dc.IPAMConfig
+
+// Len implements the sort interface for IPAMConfigs
+func (ic IPAMConfigs) Len() int { return len(ic) }
+
+// Swap implements the sort interface for IPAMConfigs
+func (ic IPAMConfigs) Swap(i, j int) { ic[i], ic[j] = ic[j], ic[i] }
+
+// Less implements the sort interface for IPAMConfigs
+func (ic IPAMConfigs) Less(i, j int) bool { return ic[i].Subnet < ic[j].Subnet }
+
+// IPAMConfigString returns a string representation of the IPAMConfigs slice
+func (ic IPAMConfigs) String() string {
+	var configStrs []string
+	for _, c := range ic {
+		configStrs = append(configStrs, ipamConfigString(c))
+	}
+	return strings.Join(configStrs, "\n")
+}
+
+func ipamConfigString(c dc.IPAMConfig) string {
+	var parts []string
+
+	if c.Subnet != "" {
+		parts = append(parts, fmt.Sprintf("subnet: %s", c.Subnet))
+	}
+
+	if c.Gateway != "" {
+		parts = append(parts, fmt.Sprintf("gateway: %s", c.Gateway))
+	}
+
+	if c.IPRange != "" {
+		parts = append(parts, fmt.Sprintf("ip_range: %s", c.IPRange))
+	}
+
+	if len(c.AuxAddress) > 0 {
+		parts = append(parts, fmt.Sprintf("aux_addresses: [%s]", mapCompareStr(c.AuxAddress)))
+	}
+
+	return strings.Join(parts, ", ")
 }
