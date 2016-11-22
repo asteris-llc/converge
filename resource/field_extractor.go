e24549f4f3248ebb79256e07a3265dc277e5de33
Rebecca Skinner <rebecca@aster.is>
2016-11-22 08:41:11 -0600

    wip

--- resource/field_extractor.go
+++ resource/field_extractor.go
@@ -16,8 +16,16 @@ package resource
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 )
+
+/* Rules for field access in structures:
+- Fields that are tagged with `export` will be exported.
+- Named structs that are tagged with `export` will be exported as a struct
+- Embedded structs will have their exported fields exported in the namespace of the containing struct
+- Embedded interfaces will not be exported, nor have their fields exported
+*/
 
 var (
 	// ErrNilStruct is returned if there is an attempt to introspect a nil value
@@ -26,6 +34,10 @@ var (
 	// ErrNonStructIntrospection is returned when attempting to introspect fields
 	// on a non-struct type.
 	ErrNonStructIntrospection = errors.New("cannot inspect non-struct fields")
+
+	// ErrFieldIndexOutOfBounds is returned when a field index is beyond the
+	// number of fields in the struct type
+	ErrFieldIndexOutOfBounds = errors.New("struct field index is out of bounds")
 )
 
 // ExportedField represents an exported field, including the containing struct,
@@ -37,35 +49,118 @@ type ExportedField struct {
 	Value         reflect.Value
 }
 
-// ExportedFields returns a slice of fields that have been exported from a
-// struct, along with the name.
-func ExportedFields(input interface{}) (exported []*ExportedField, err error) {
+func newExportedField(input interface{}, index int) (*ExportedField, bool) {
 	if nil == input {
-		return exported, ErrNilStruct
+		fmt.Println("newExportedField: input is nil")
+		return nil, false
 	}
 	val, err := getStruct(reflect.ValueOf(input))
 	if err != nil {
+		fmt.Println("newExportedField: cannot resolve input to a struct type: ", err)
+		return nil, false
+	}
+
+	if index >= val.Type().NumField() {
+		fmt.Println("newExportedField: index out of bounds (", index, " > ", val.Type().NumField(), ")")
+		return nil, false
+	}
+	fieldType := val.Type().Field(index)
+	fieldVal := val.Field(index)
+	exportedName, ok := fieldType.Tag.Lookup("export")
+	if !ok {
+		fmt.Printf("newExportedField: %T.%s: field is not exported\n", input, fieldType.Name)
+		return nil, false
+	}
+	return &ExportedField{
+		FieldName:     fieldType.Name,
+		ReferenceName: exportedName,
+		Value:         fieldVal,
+	}, true
+}
+
+// ExportedFields returns a slice of fields that have been exported from a
+// struct, along with the name.
+func ExportedFields(input interface{}) (exported []*ExportedField, err error) {
+	var embeddedExports []*ExportedField
+	if nil == input {
+		return exported, ErrNilStruct
+	}
+	asStruct, err := getStruct(reflect.ValueOf(input))
+	if err != nil {
 		return exported, err
 	}
-
-	typ := val.Type()
-	for i := 0; i < typ.NumField(); i++ {
-		field := typ.Field(i)
-		fval := val.Field(i)
-		refName, ok := field.Tag.Lookup("export")
-
-		if !ok {
+	for i := 0; i < asStruct.Type().NumField(); i++ {
+		isAnon, anonErr := fieldIsAnonymous(input, i)
+		if anonErr != nil {
+			return exported, anonErr
+		}
+		if isAnon {
+			isKind, kindErr := fieldIsKind(input, i, reflect.Struct)
+			if kindErr != nil {
+				return exported, kindErr
+			}
+			if !isKind {
+				continue
+			}
+			fromEmbedded, err := ExportedFields(input)
+			if err != nil {
+				return exported, err
+			}
+			embeddedExports = append(embeddedExports, fromEmbedded...)
 			continue
 		}
-		exportedField := &ExportedField{
-			FieldName:     field.Name,
-			ReferenceName: refName,
-			Value:         fval,
+		fmt.Println("non-anonymous field at idx: ", i)
+		if field, ok := newExportedField(input, i); ok {
+			fmt.Println("\t generated field, appending to exported list")
+			exported = append(exported, field)
 		}
-		exported = append(exported, exportedField)
 	}
-
 	return exported, nil
+}
+
+func getFieldKind(input interface{}, index int) (reflect.Kind, error) {
+	if input == nil {
+		return reflect.Invalid, ErrNilStruct
+	}
+	asStruct, err := getStruct(reflect.ValueOf(input))
+	if err != nil {
+		return reflect.Invalid, err
+	}
+	if index >= asStruct.Type().NumField() {
+		return reflect.Invalid, ErrFieldIndexOutOfBounds
+	}
+	fieldType := asStruct.Type().Field(index).Type
+	for fieldType.Kind() == reflect.Ptr {
+		fieldType = fieldType.Elem()
+	}
+	return fieldType.Kind(), nil
+}
+
+func fieldIsKind(input interface{}, index int, kinds ...reflect.Kind) (bool, error) {
+	for _, kind := range kinds {
+		actualKind, err := getFieldKind(input, index)
+		if err != nil {
+			return false, err
+		}
+		if kind == actualKind {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func fieldIsAnonymous(input interface{}, index int) (bool, error) {
+	if input == nil {
+		return false, ErrNilStruct
+	}
+	asStruct, err := getStruct(reflect.ValueOf(input))
+	if err != nil {
+		return false, err
+	}
+	if index >= asStruct.Type().NumField() {
+		return false, ErrFieldIndexOutOfBounds
+	}
+	return asStruct.Type().Field(index).Anonymous, nil
 }
 
 func getStruct(val reflect.Value) (reflect.Value, error) {
