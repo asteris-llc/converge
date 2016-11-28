9ca8a74c0ac7c56e462b626b99ff36a34f2fa4f1
Allison Richardet <allison@aster.is>
2016-11-28 15:24:51 -0600

    Remove indent for multi-line diffs

--- prettyprinters/human/human.go
+++ prettyprinters/human/human.go
@@ -196,9 +196,9 @@ func (p *Printer) diff(before, after string) (string, error) {
 	}
 
 	tmpl, err := p.template(`before:
-{{indent .Before}}
+{{.Before}}
 after:
-{{indent .After}}`)
+{{.After}}`)
 	if err != nil {
 		return "", err
 	}
