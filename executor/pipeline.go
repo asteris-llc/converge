a97fe3a9a4d0ee2ac25f37a56c604dec0abc961d
Rebecca Skinner <rebecca@aster.is>
2016-11-30 11:31:00 -0600

    fix tests and remove debug comments

--- executor/pipeline.go
+++ executor/pipeline.go
@@ -14,11 +14,7 @@
 
 package executor
 
-import (
-	"fmt"
-
-	"golang.org/x/net/context"
-)
+import "golang.org/x/net/context"
 
 // PipelineFunc represents a pipelined function that uses multi-return instead
 // of either.
@@ -46,11 +42,7 @@ func (p Pipeline) AndThen(f PipelineFunc) Pipeline {
 // AndThen with printf
 func (p Pipeline) LogAndThen(msg string, f PipelineFunc) Pipeline {
 	logger := func(c context.Context, i interface{}) (interface{}, error) {
-		fmt.Println("\t Calling: ", msg)
 		result, err := f(c, i)
-		fmt.Printf("\t\t Results:\n")
-		fmt.Printf("\t\t\t result :: %T; %v\n", result, result)
-		fmt.Printf("\t\t\t error: %v\n", err)
 		return result, err
 	}
 	p.CallStack = append(p.CallStack, logger)
