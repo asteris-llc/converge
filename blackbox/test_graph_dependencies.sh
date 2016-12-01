3ad85368bfe24a43527a73a5eb05f0cbf56842a6
Brian Hicks <brian@brianthicks.com>
2016-12-01 08:25:12 -0600

    blackbox(test_graph_dependencies): only start server once

--- blackbox/test_graph_dependencies.sh
+++ blackbox/test_graph_dependencies.sh
@@ -3,9 +3,13 @@ set -eo pipefail
 
 ROOT=$(pwd)
 
+"$ROOT"/converge server --no-token &
+PID=$!
+
 TMP=$(mktemp -d -t converge.graph_dependencies.XXXXXXXXXX)
 function finish {
     rm -rf "$TMP"
+    kill -2 "$PID"
 }
 trap finish EXIT
 
@@ -15,7 +19,7 @@ for src in "$ROOT"/samples/testdata/*.hcl; do
   b=$(basename "$src")
 	# since panic doesn't happen 100% of the time, run test a few times
   for _ in $(seq 1 5); do
-    "$ROOT"/converge graph --local -l "WARN" "$src" >/dev/null
+    "$ROOT"/converge graph -l "WARN" "$src" >/dev/null
 	done
 	echo "success: no errors generating dependencies for ${b}"
 done
