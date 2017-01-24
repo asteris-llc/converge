9f5b059bb00245ad0670e032c19c330969da7ec8
Rebecca Skinner <rebecca@aster.is>
2017-01-24 10:25:58 -0600

    remove unnecessary dbus wrappers and add type tests

--- resource/systemd/unit/types_test.go
+++ resource/systemd/unit/types_test.go
@@ -12,36 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package unit_test
+package unit
 
 import (
 	"strings"
 	"testing"
 
-	"github.com/asteris-llc/converge/resource/systemd/unit"
 	"github.com/stretchr/testify/assert"
 )
 
-var typeMap = map[unit.UnitType]string{
-	unit.UnitTypeService:   "service",
-	unit.UnitTypeSocket:    "socket",
-	unit.UnitTypeDevice:    "device",
-	unit.UnitTypeMount:     "mount",
-	unit.UnitTypeAutoMount: "automount",
-	unit.UnitTypeSwap:      "swap",
-	unit.UnitTypeTarget:    "target",
-	unit.UnitTypePath:      "path",
-	unit.UnitTypeTimer:     "timer",
-	unit.UnitTypeSnapshot:  "snapshot",
-	unit.UnitTypeSlice:     "slice",
-	unit.UnitTypeScope:     "scope",
-	unit.UnitTypeUnknown:   "",
+var typeMap = map[UnitType]string{
+	UnitTypeService:   "service",
+	UnitTypeSocket:    "socket",
+	UnitTypeDevice:    "device",
+	UnitTypeMount:     "mount",
+	UnitTypeAutoMount: "automount",
+	UnitTypeSwap:      "swap",
+	UnitTypeTarget:    "target",
+	UnitTypePath:      "path",
+	UnitTypeTimer:     "timer",
+	UnitTypeSnapshot:  "snapshot",
+	UnitTypeSlice:     "slice",
+	UnitTypeScope:     "scope",
+	UnitTypeUnknown:   "",
 }
 
-// TestUnitTypeZeroValue ensures that the zero value is unit.UnitTypeUnknown
+// TestUnitTypeZeroValue ensures that the zero value is UnitTypeUnknown
 func TestUnitTypeZeroValue(t *testing.T) {
-	var u unit.UnitType
-	assert.Equal(t, unit.UnitTypeUnknown, u)
+	var u UnitType
+	assert.Equal(t, UnitTypeUnknown, u)
 }
 
 // Test conversion from a unit file name or service name
@@ -50,40 +49,40 @@ func TestUnitTypeFromName(t *testing.T) {
 	t.Run("when-name", func(t *testing.T) {
 		t.Parallel()
 		for k, v := range typeMap {
-			assert.Equal(t, k, unit.UnitTypeFromName(v))
+			assert.Equal(t, k, UnitTypeFromName(v))
 		}
 	})
 	t.Run("when-dot-name", func(t *testing.T) {
 		t.Parallel()
 		for k, v := range typeMap {
 			name := "." + v
-			assert.Equal(t, k, unit.UnitTypeFromName(name))
+			assert.Equal(t, k, UnitTypeFromName(name))
 		}
 	})
 	t.Run("when-path", func(t *testing.T) {
 		t.Parallel()
 		for k, v := range typeMap {
 			name := "/foo/bar/baz/name." + v
-			assert.Equal(t, k, unit.UnitTypeFromName(name))
+			assert.Equal(t, k, UnitTypeFromName(name))
 		}
 	})
 	t.Run("when-dashes", func(t *testing.T) {
 		t.Parallel()
 		s := "sys-devices-platform-serial8250-tty-ttyS16.device"
-		assert.Equal(t, unit.UnitTypeDevice, unit.UnitTypeFromName(s))
+		assert.Equal(t, UnitTypeDevice, UnitTypeFromName(s))
 	})
 	t.Run("when-upper-case", func(t *testing.T) {
 		t.Parallel()
 		for k, v := range typeMap {
-			assert.Equal(t, k, unit.UnitTypeFromName(strings.ToUpper(v)))
+			assert.Equal(t, k, UnitTypeFromName(strings.ToUpper(v)))
 		}
 	})
 	t.Run("when-error", func(t *testing.T) {
 		t.Parallel()
-		assert.Equal(t, unit.UnitTypeUnknown, unit.UnitTypeFromName("unknown-service"))
-		assert.Equal(t, unit.UnitTypeUnknown, unit.UnitTypeFromName(""))
-		assert.Equal(t, unit.UnitTypeUnknown, unit.UnitTypeFromName("service1"))
-		assert.Equal(t, unit.UnitTypeUnknown, unit.UnitTypeFromName("service.service1"))
+		assert.Equal(t, UnitTypeUnknown, UnitTypeFromName("unknown-service"))
+		assert.Equal(t, UnitTypeUnknown, UnitTypeFromName(""))
+		assert.Equal(t, UnitTypeUnknown, UnitTypeFromName("service1"))
+		assert.Equal(t, UnitTypeUnknown, UnitTypeFromName("service.service1"))
 	})
 }
 
@@ -96,23 +95,45 @@ func TestSuffix(t *testing.T) {
 
 func TestUnitTypeString(t *testing.T) {
 	t.Parallel()
-	typeMap := map[unit.UnitType]string{
-		unit.UnitTypeService:   "Service",
-		unit.UnitTypeSocket:    "Socket",
-		unit.UnitTypeDevice:    "Device",
-		unit.UnitTypeMount:     "Mount",
-		unit.UnitTypeAutoMount: "Automount",
-		unit.UnitTypeSwap:      "Swap",
-		unit.UnitTypeTarget:    "Target",
-		unit.UnitTypePath:      "Path",
-		unit.UnitTypeTimer:     "Timer",
-		unit.UnitTypeSnapshot:  "Snapshot",
-		unit.UnitTypeSlice:     "Slice",
-		unit.UnitTypeScope:     "Scope",
-		unit.UnitTypeUnknown:   "",
+	typeMap := map[UnitType]string{
+		UnitTypeService:   "Service",
+		UnitTypeSocket:    "Socket",
+		UnitTypeDevice:    "Device",
+		UnitTypeMount:     "Mount",
+		UnitTypeAutoMount: "Automount",
+		UnitTypeSwap:      "Swap",
+		UnitTypeTarget:    "Target",
+		UnitTypePath:      "Path",
+		UnitTypeTimer:     "Timer",
+		UnitTypeSnapshot:  "Snapshot",
+		UnitTypeSlice:     "Slice",
+		UnitTypeScope:     "Scope",
+		UnitTypeUnknown:   "",
 	}
 
 	for k, v := range typeMap {
 		assert.Equal(t, v, k.UnitTypeString())
+	}
+}
+
+func TestUnitHasProperties(t *testing.T) {
+	typeMap := map[UnitType]bool{
+		UnitTypeTarget:    false,
+		UnitTypeSnapshot:  false,
+		UnitTypeUnknown:   false,
+		UnitTypeService:   true,
+		UnitTypeSocket:    true,
+		UnitTypeDevice:    true,
+		UnitTypeMount:     true,
+		UnitTypeAutoMount: true,
+		UnitTypeSwap:      true,
+		UnitTypePath:      true,
+		UnitTypeTimer:     true,
+		UnitTypeSlice:     true,
+		UnitTypeScope:     true,
+	}
+
+	for k, v := range typeMap {
+		assert.Equal(t, v, k.HasProperties())
 	}
 }
